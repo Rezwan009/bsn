9be5cd80403d551bbad880ef4d5710630095ed6c
Rezwan Ahmed <rezwan.ahmed@ibcs-primax.com>
2026-01-02 21:22:03 +0600

    Token generated successfully and login functionality done

--- frontend/book-network-ui/src/app/pages/login/login.component.ts
+++ frontend/book-network-ui/src/app/pages/login/login.component.ts
@@ -1,26 +1,47 @@
+import { AuthenticationService } from 'src/app/services/services';
 import { AuthenticationRequest } from './../../services/models/authentication-request';
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+import { TokenService } from 'src/app/services/token/token.service';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
-  styleUrls: ['./login.component.scss']
+  styleUrls: ['./login.component.scss'],
 })
 export class LoginComponent {
-
   authRequest: AuthenticationRequest = { email: '', password: '' };
   errorMsg: Array<String> = [];
-
   constructor(
-    
-  ) {
-
-  }
-
+    private authenticationService: AuthenticationService,
+    private router: Router,
+    private tokenService: TokenService
+  ) {}
 
   login() {
+    this.errorMsg = [];
 
+    this.authenticationService
+      .authenticate({
+        body: this.authRequest,
+      })
+      .subscribe({
+        next: (res) => {
+          this.tokenService.token = res.token as string;
+          this.router.navigate(['books']);
+        },
+        error: (err) => {
+          if (err.error.validationErrors) {
+            this.errorMsg = err.error.validationErrors;
+          } else {
+            this.errorMsg.push(err.error.error);
+          }
+          console.log(err);
+        },
+      });
   }
 
-  register(){}
+  register() {
+    this.router.navigate(['register']);
+  }
 }
